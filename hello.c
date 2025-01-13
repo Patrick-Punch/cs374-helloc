4826ff595c0706122433f979e090ccac58cbe217
Patrick Punch <punchp@oregonstate.edu>
2025-01-13 11:00:44 -0800

    added alternate methods for printing

--- hello.c
+++ hello.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
+    char x[128] = "Hello World!\n";
+    char *y = "Hello World!\n";
+
     write(1, "Hello World!\n", 14);
+    write(1, x, sizeof(x));
+    write(1, y, 14);
 }
