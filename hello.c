a0c6427466fc77e8d72e6244de774e9f3046e337
Patrick Punch <punchp@oregonstate.edu>
2025-01-13 11:36:08 -0800

    added pointer example from class

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
+#include <stdio.h>
 
+void foo(int *x, int *y) // x = &a, y = &b
+{
+    (*x)++;
+    (*y)++;
+}
 int main(void)
 {
     char x[128] = "Hello World!\n";
@@ -8,4 +14,10 @@ int main(void)
     write(1, "Hello World!\n", 14);
     write(1, x, sizeof(x));
     write(1, y, 14);
+
+
+    int a = 12, b = 24;
+    foo(&a, &b);
+    printf("%d %d\n", a, b);
 }
+
